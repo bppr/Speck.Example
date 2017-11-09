94541112d6e51bef1b8d1eaa86fb1034bf9e88de
Brian Pratt <brian@8thlight.com>
2017-11-09 15:59:02 -0600

    updates to use Speck@0.1.0

--- Package.swift
+++ Package.swift
@@ -1,8 +1,15 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-	name: "Speck",
-	dependencies: [
-		.Package(url: "https://github.com/bppr/Speck", Version(0, 0, 2))
-	]
+  name: "SpeckExample",
+  products: [],
+  dependencies: [
+    .package(url: "../Speck", from: "0.1.0"),
+    // .package(url: "https://github.com/bppr/Speck", from: "0.1.0"),
+  ],
+  targets: [
+    .target(name: "Example", dependencies: ["Speck"])
+  ]
 )
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Speck.Example
 ===============
 
+`swift run example` to run the example :)
+
 Please see [Speck](https://github.com/bppr/Speck)
